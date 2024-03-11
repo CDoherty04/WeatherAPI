2b0ad678cb3ced9a1fda3d4395f5d747ba68d747
Charlie <charlieedoherty@gmail.com>
2024-03-11 13:41:33 -0500

    Utilize pandas to access csv files and provide accurate temperature data for a certain date and station.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -10,7 +11,12 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def access(station, date):
-    temperature = 1
+
+    # Create a dataframe based on csv files
+    filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
+
     return {"station": station,
             "date": date,
             "temperature": temperature}
